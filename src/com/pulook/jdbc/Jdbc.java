af10c9484977f5213268baef7efce0a570bd4581
general <463734522@qq.com>
2013-10-09 00:14:36 +0800

    加上了JDBC，这个JDBC，之后还没有完整准备加上拦截器，自己写的拦截器

---
+++ src/com/pulook/jdbc/Jdbc.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.pulook.jdbc;
+
+import java.sql.Connection;
+
+/**
+ * @author 周大帅	
+ * @email 463734522@qq.com
+ * 2013年10月9日
+ */
+public class Jdbc {
+	private static Connection connection;
+
+}
